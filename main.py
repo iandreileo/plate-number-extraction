39549fc50a5d85e712fd713a07806be3b3466ecb
Ilie Andrei-Leonard <office@inntech.dev>
2022-12-19 15:31:07 +0200

    Curatenie

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from PIL import Image
-from PIL import ImageFilter
-
-import cv2
 import math
-
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -25,7 +20,6 @@ def convolution(image, kernel, average=False):
     padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
 
     # Iteram prin toti pixelii
-
     for row in range(image_row):
         for col in range(image_col):
             output[row, col] = np.sum(kernel * padded_image[row:row + kernel_row, col:col + kernel_col])
@@ -43,15 +37,14 @@ def sobel_edge_detection(image, xfilter, yfilter):
     # Aplicam convolutie pe imagine vertical (transpus)
     new_image_y = convolution(image, yfilter)
 
-    # Calculam gradientul
-
+    # Calculam gradientul pe filtrele orizontal si vertical
     gradient_magnitude = np.sqrt(np.square(new_image_x) + np.square(new_image_y))
-
     gradient_magnitude *= 255.0 / gradient_magnitude.max()
 
     # Returnam magnitudinea gradientului
     return gradient_magnitude
 
+# Functie auxiliara pentru normala
 def dnorm(x, mu, sd):
     return 1 / (np.sqrt(2 * np.pi) * sd) * np.e ** (-np.power((x - mu) / sd, 2) / 2)
 
@@ -71,25 +64,28 @@ def gaussian_blur(image, kernel_size):
 
 def main():
     # Citim poza
-    img = cv2.imread('test3.jpg')
+    img = cv2.imread('poze/test2.jpg')
 
-    # Aplicam grayscale
+    # Aplicam filtru de grayscale
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Declaram filtru
+    # Declaram filtrele ca in labul de SPG
     xfilter = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     yfilter = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
 
+    # Aplicam filtrul gausian pentru blur
+    # Pentru a indeparta "zgomotul"
     img = gaussian_blur(img, 2)
 
     # Facem edge detection folosind sobel de la SPG
+    # Cu 2 filtre orizontal si vertical
     img = sobel_edge_detection(img, xfilter, yfilter)
 
-    # Invert
+    # Inversam culorile din imagine pentru claritate mai buna
     mask = np.full(img.shape, 255)
     img = mask - img
     img = img.astype(np.uint8)
-    
+
     # Scriem imaginea inapoi in fisier
     cv2.imwrite("test_rezolvat.jpg", img)
 
