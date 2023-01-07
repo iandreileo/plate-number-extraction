5241a1e2808e2b96c91611f743c17c2a16215b61
Ilie Andrei-Leonard <office@inntech.dev>
2023-01-07 11:23:09 +0200

    Comentarii adaugate

--- proiect_corect/canny.py
+++ proiect_corect/canny.py
@@ -39,6 +39,8 @@ class Canny:
 
 
     def gen_gaussian_kernel(self, k_size, sigma):
+        # Calculam kernelul gaussian conform teoriei
+        # Sursa(1): https://pages.stat.wisc.edu/~mchung/teaching/MIA/reading/diffusion.gaussian.kernel.pdf
         center = k_size // 2
         x, y = mgrid[0 - center : k_size - center, 0 - center : k_size - center]
         g = 1 / (2 * pi * sigma) * exp(-(square(x) + square(y)) / (2 * square(sigma)))
@@ -46,12 +48,16 @@ class Canny:
 
 
     def gaussian_filter(self, image, k_size, sigma):
+
+        # Extragem inaltime si latimea imaginii
         height, width = image.shape[0], image.shape[1]
-        # dst image height and width
+        
+        # Calculam latimea si inaltime
+        # Luand in considerare parametrul de nucleu furnizat
         dst_height = height - k_size + 1
         dst_width = width - k_size + 1
 
-        # im2col, turn the k_size*k_size pixels into a row and np.vstack all rows
+        # Construim imaginea conform teoriei
         image_array = zeros((dst_height * dst_width, k_size * k_size))
         row = 0
         for i, j in product(range(dst_height), range(dst_width)):
@@ -59,48 +65,85 @@ class Canny:
             image_array[row, :] = window
             row += 1
 
-        #  turn the kernel into shape(k*k, 1)
+        # Transformam kernelul intr-o forma (k patrat, 1)
         gaussian_kernel = self.gen_gaussian_kernel(k_size, sigma)
         filter_array = ravel(gaussian_kernel)
 
-        # reshape and get the dst image
+        # Facem produsul celor 2: imagine si filtrul
+        # Facem reshape
+        # Transformam rezultatul in 8 canale
         dst = dot(image_array, filter_array).reshape(dst_height, dst_width).astype(uint8)
 
+        # Returnam
         return dst
 
     def blur(self, image):
-        # image = cv2.GaussianBlur(image, (5, 5), 0)
         image = self.gaussian_filter(image, 5, sigma=0.8)
 
         return image
 
     def sobel(self, image):
+        # Aplicam pe imagine grayscale
+        # Apoi ii aplicam un gaussian blur
+        # Aceste 2 operatii pentru netezirea imaginii
         image = self.blur(self.grayscale(image))
+
+        # Initializm o matrice de convolutie
+        # de marimea imaginii, dar cu pixelii setati pe 0
         convolved = np.zeros(image.shape)
+
+        # Initializam 2 matrici
+        # Una petru orizontal, una pentru vertical
+        # Sursa(1): https://en.wikipedia.org/wiki/Sobel_operator
+        # Sursa(2): Lab SPG
         G_x = np.zeros(image.shape)
         G_y = np.zeros(image.shape)
+        
+        # Extragem tuplul cu marimea imaginii
         size = image.shape
+
+        # Declaram la fel ca mai sus
+        # 2 matrici orizontal, si vertical pentru kernenul filtrului 
+        # Sursa(1): Lab SPG
+        # Sursa(2): Implementare matlab https://en.wikipedia.org/wiki/Sobel_operator
         kernel_x = np.array(([-1, 0, 1], [-2, 0, 2], [-1, 0, 1]))
         kernel_y = np.array(([-1, -2, -1], [0, 0, 0], [1, 2, 1]))
+
+
+        # Completam matricile orizontale si verticale
+        # Conform teoriei
+        # https://en.wikipedia.org/wiki/Sobel_operator
         for i in range(1, size[0] - 1):
             for j in range(1, size[1] - 1):
                 G_x[i, j] = np.sum(np.multiply(image[i - 1 : i + 2, j - 1 : j + 2], kernel_x))
                 G_y[i, j] = np.sum(np.multiply(image[i - 1 : i + 2, j - 1 : j + 2], kernel_y))
         
+        # Pentru a forma imaginea la loc
+        # Vom folosi operatia de convolutie conform teoriei
         convolved = np.sqrt(np.square(G_x) + np.square(G_y))
         convolved = np.multiply(convolved, 255.0 / convolved.max())
 
         angles = np.rad2deg(np.arctan2(G_y, G_x))
         angles[angles < 0] += 180
+
+        # Transformam imaginea in 8 canale
         convolved = convolved.astype('uint8')
+
+        # Returnam imaginea si unghiurile
         return convolved, angles
 
     def nms(self, image, angles):
+        # Extragem tuplul cu dimensiunile
         size = image.shape
+
+        # Initializam o matrice de
+        # dimensiunea imaginii
         suppressed = np.zeros(size)
+
+        # Iteram prin pixelii imaginii
+        # Si incercam sa gasim 
         for i in range(1, size[0] - 1):
             for j in range(1, size[1] - 1):
-                # print(angles[i,j])
                 if (0 <= angles[i, j] < 22.5) or (157.5 <= angles[i, j] <= 180):
                     value_to_compare = max(image[i, j - 1], image[i, j + 1])
                 elif (22.5 <= angles[i, j] < 67.5):
@@ -112,22 +155,41 @@ class Canny:
                 
                 if image[i, j] >= value_to_compare:
                     suppressed[i, j] = image[i, j]
+
+        # 
         suppressed = np.multiply(suppressed, 255.0 / suppressed.max())
+
+
         return suppressed
 
     def double_threshold_hysteresis(self, image, low, high):
+
+        # Definim tresholdul minim si maxim
         weak = 50
         strong = 255
+
+        # Extragm tuplul cu dimensiunile imaginii
         size = image.shape
+
+        # Initializam rezultatul ca fiind o matrice
+        # De dimensiunea imaginii initiale
         result = np.zeros(size)
+
+        # Extragem edge-urile care se incadreaza la weak si storng
         weak_x, weak_y = np.where((image > low) & (image <= high))
         strong_x, strong_y = np.where(image >= high)
+
+        # Setam in matricea rezultat
+        # Calculele anterioare
         result[strong_x, strong_y] = strong
         result[weak_x, weak_y] = weak
+
+        # Definim directiile orizontale si verticale
         dx = np.array((-1, -1, 0, 1, 1, 1, 0, -1))
         dy = np.array((0, 1, 1, 1, 0, -1, -1, -1))
         size = image.shape
         
+        # Calculam edge-urile care raman in imagine
         while len(strong_x):
             x = strong_x[0]
             y = strong_y[0]
@@ -140,14 +202,26 @@ class Canny:
                     result[new_x, new_y] = strong
                     np.append(strong_x, new_x)
                     np.append(strong_y, new_y)
+        
         result[result != strong] = 0
         return result
 
     def canny(self, image, low, high):
+
+        # Pentru a aplica filtrul CANNY
+        # Trebuie sa urmam pasii de mai jos:
+
+        # Aplicam sobel pe imagine
         image, angles = self.sobel(image)
+        
+        # Aplicam NMS (non-maxima supression)
+        # Pentru a minimiza contururile ne-necesare
         image = self.nms(image, angles)
-        gradient = np.copy(image)
+                
+        # Aplicam Hysteresis thresholding
+        # Pentru a netezi imaginea si a scoate in evidenta contururile
         image = self.double_threshold_hysteresis(image, low, high)
+        
         return image
 
     def ocr(self, ROI):
@@ -192,11 +266,15 @@ class Canny:
 
     def extract_plate_number(self, image):
 
+        # Generam imaginea folosind canny
         new_image = self.canny(image, 0, 50)
 
+        # Setam sa fie pe 8 canale
+        # Ca sa arate bine pe imshow
         new_image = new_image.astype(np.uint8)
 
         cv2.imshow("Sobel Image", new_image)
         cv2.waitKey(0)
 
+        # Returnam si afisam numarul de inamtriculare
         return self.find_contours(image, new_image)
